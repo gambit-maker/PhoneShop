a2339bcfab958ef8c1cc7b87ba4ecad05322548e
HP <quanghuy12c6@gmail.com>
2021-01-12 12:22:02 +0700

    v3

--- index.js
+++ index.js
@@ -35,4 +35,60 @@ $(document).ready(function () {
         $grid.isotope({ filter: filterValue });
     });
 
+
+    // New phone carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            },
+        }
+    });
+
+    //Blogs owl-carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    });
+
+    //Product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    // let $input = $(".qty .qty_input")
+    //Click on qty button
+    $qty_up.click(function(e){        
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);        
+        if ($input.val() >= 1 && $input.val() < 10) {
+            $input.val(function(i,oldval){
+                return ++oldval;
+            })            
+        }
+    });
+    $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if ($input.val() > 1 && $input.val() <= 10) {
+            $input.val(function(i,oldval){
+                return --oldval;
+            })            
+        }
+    });
 });
+
